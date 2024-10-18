3ef1f869b024584302c56776e2c1197bd8559ed3
rosemolt <rosemolthomas02@gmail.com>
2024-10-17 22:06:32 -0700

    Completed

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp();
+}
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:
+      false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+              "Calculator App",
+          style: TextStyle(
+            color: Colors.indigo
+          ),
+          ),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: ,
+      ),
+    );
+  }
+}
